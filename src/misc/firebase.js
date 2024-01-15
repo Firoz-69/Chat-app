de5830f72820fb688c40d6d7669c97c8d20bbc2e
Firoz-69 <gokulnathg1205@gmail.com>
2024-01-15 17:53:59 +0530

    firebase

---
+++ src/misc/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase/app';
+
+const Config = {
+  apiKey: 'AIzaSyCSk9xdTqqv_hSTtyHc9mMJx7AKFxxwCLo',
+  authDomain: 'chat-web-app-ab987.firebaseapp.com',
+  projectId: 'chat-web-app-ab987',
+  storageBucket: 'chat-web-app-ab987.appspot.com',
+  messagingSenderId: '761878762787',
+  appId: '1:761878762787:web:0073917702d19a731b2896',
+};
+const app = firebase.initializeApp(Config);
